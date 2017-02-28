ac7b0f6c78734c786be8890cbb3362aeded563f7
billkoski04 <Bill@D-128-95-0-161.dhcp4.washington.edu>
2017-02-27 20:47:23 -0800

    Updated 2-27-17

--- app/src/main/java/com/example/bill/downloadimage/MainActivity.java
+++ app/src/main/java/com/example/bill/downloadimage/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.bill.downloadimage;
 
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,6 +11,7 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.storage.FileDownloadTask;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import java.io.File;
 import java.io.IOException;
@@ -22,12 +24,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Set up storage reference called storageRef
         StorageReference storageRef = FirebaseStorage.getInstance().getReference();
         storageRef = storageRef.child("image.jpeg");
 
         File localFile;
 
-
+        //Download file from firebase
         try{
             localFile = File.createTempFile("image", "jpeg");
             FileDownloadTask fileDownloadTask = storageRef.getFile(localFile);
@@ -52,5 +55,27 @@ public class MainActivity extends AppCompatActivity {
             Log.e("download",e.getMessage());
 
         }
+
+        //Upload file to firebase
+        Uri file = Uri.parse("android.resource://com.example.bill.downloadimage/drawable/eclipse");
+
+
+        storageRef.putFile(file)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        // Get a URL to the uploaded content
+                        Uri downloadUrl = taskSnapshot.getDownloadUrl();
+                        Log.d("upload", "Super extra awesome success");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        // Handle unsuccessful uploads
+                        // ...
+                        Log.d("upload", "Super extra sad failure");
+                    }
+                });
     }
 }
